2a53caafefce421e0b46f20e78d96d1b2b2a5872
akorede <emiolasaheed1@gmail.com>
2023-10-12 11:18:14 +0100

    Fix linters errors

--- solver.rb
+++ solver.rb
@@ -1,15 +1,11 @@
 class Solver
- 
   def factorial(int)
+    raise ArgumentError, 'Cannot find the factorial of a negative number' if int.negative?
 
-    if int < 0
-      raise ArgumentError, "Cannot find the factorial of a negative number"
-    end
     result = 1
-    for i in result..int do
+    (result..int).each do |i|
       result *= i
     end
     result
   end
-
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,18 +3,18 @@ require './solver'
 
 describe Solver do
   subject { Solver.new }
-  context "#factorial" do
-    it "return the factorial for a number" do
+  context '#factorial' do
+    it 'return the factorial for a number' do
       expect(subject.factorial(3)).to eq(6)
       expect(subject.factorial(5)).to eq(120)
     end
 
-    it "return one when number is zero" do
+    it 'return one when number is zero' do
       expect(subject.factorial(0)).to eq(1)
     end
 
-    it "return one when number is zero" do
-      expect(subject.factorial(-1)).to raise (ArgumentError)
+    it 'return one when number is zero' do
+      expect(subject.factorial(-1)).to raise(ArgumentError)
     end
   end
 end
