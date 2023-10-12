c69c1974b14dac2b23fd6ea67ddec817aec79644
akorede <emiolasaheed1@gmail.com>
2023-10-12 10:40:06 +0100

    Test the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require './solver'
 
 describe Solver do
-  
+  subject { Solver.new }
+  context "#factorial" do
+    it "return the factorial for a number" do
+      expect(subject.factorial(3)).to eq(6)
+      expect(subject.factorial(5)).to eq(120)
+    end
+  end
 end
