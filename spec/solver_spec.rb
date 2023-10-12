25054212ce2a01a271e0f67702b6fb093c0aadbf
akorede <emiolasaheed1@gmail.com>
2023-10-12 10:56:52 +0100

    Raise exception error when number is negative

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,5 +8,13 @@ describe Solver do
       expect(subject.factorial(3)).to eq(6)
       expect(subject.factorial(5)).to eq(120)
     end
+
+    it "return one when number is zero" do
+      expect(subject.factorial(0)).to eq(1)
+    end
+
+    it "raises an exception when number is less than zero" do
+      expect { subject.factorial(-1) }.to raise (ArgumentError)
+    end
   end
 end
